64a0344549a058ce345b98bc99f0e1483d7fcd43
Giant09dev <aidimi2810.sgn@gmail.com>
2024-12-25 19:31:03 +0700

    .

--- final.js
+++ final.js
@@ -87,58 +87,55 @@ const tree_loader = new GLTFLoader();
 // Hàm tải mô hình cây
 const TreeCache = [];
 
-const Tree_links = [
-  "/GLB_Models/tree_1.glb",
-  "/GLB_Models/tree_2.glb",
-];
+const Tree_links = ["/GLB_Models/tree_1.glb", "/GLB_Models/tree_2.glb"];
 
-function loadTreeModel(Tree_links) {  
-  return new Promise((resolve, reject) => {  
-    if (Tree_links.length === 0) {  
-      console.log('Không tìm thấy mô hình');  
-      resolve();  
-      return;  
-    }  
+function loadTreeModel(Tree_links) {
+  return new Promise((resolve, reject) => {
+    if (Tree_links.length === 0) {
+      console.log("Không tìm thấy mô hình");
+      resolve();
+      return;
+    }
 
-    const promises = Tree_links.map(  
-      (treeLinks) =>  
-        new Promise((resolve) => {  
-          tree_loader.load(  
-            treeLinks,  
-            (gltf) => {  
-              let trees = new THREE.Object3D();  
-              trees.add(gltf.scene);  
-              trees.scale.set(0.7, 0.7, 0.7);  
-              TreeCache.push(trees);  
+    const promises = Tree_links.map(
+      (treeLinks) =>
+        new Promise((resolve) => {
+          tree_loader.load(
+            treeLinks,
+            (gltf) => {
+              let trees = new THREE.Object3D();
+              trees.add(gltf.scene);
+              trees.scale.set(0.7, 0.7, 0.7);
+              TreeCache.push(trees);
 
-              // Tạo AnimationMixer nếu mô hình có animation
-              let mixer = new THREE.AnimationMixer(trees);
-              mixers.push(mixer);
+              // // Tạo AnimationMixer nếu mô hình có animation
+              // let mixer = new THREE.AnimationMixer(trees);
+              // mixers.push(mixer);
 
-              // Duyệt qua các hoạt hình trong mô hình (nếu có) và thêm chúng vào mixer
-              gltf.animations.forEach((clip) => {
-                if (mixer) mixer.clipAction(clip).play();
-              });
+              // // Duyệt qua các hoạt hình trong mô hình (nếu có) và thêm chúng vào mixer
+              // gltf.animations.forEach((clip) => {
+              //   if (mixer) mixer.clipAction(clip).play();
+              // });
 
-              resolve();  
-            }, 
-            undefined,  
-            (error) => {  
-              console.error("Lỗi khi tải mô hình:", error);  
-              resolve();  
-            }  
+              resolve();
+            },
+            undefined,
+            (error) => {
+              console.error("Lỗi khi tải mô hình:", error);
+              resolve();
+            }
           );
-        })  
-    );  
+        })
+    );
 
-    Promise.all(promises).then(() => {  
-      console.log("Tất cả mô hình tree đã được tải:");  
-      TreeCache.forEach((object, index) => {  
-        console.log(`Phần tử tree ${index}: ${object.position.x}`, object);  
-      });  
-      resolve();  
-    });   
-  });  
+    Promise.all(promises).then(() => {
+      console.log("Tất cả mô hình tree đã được tải:");
+      TreeCache.forEach((object, index) => {
+        console.log(`Phần tử tree ${index}: ${object.position.x}`, object);
+      });
+      resolve();
+    });
+  });
 }
 
 // Hàm chọn ngẫu nhiên bên trái hoặc phải và mô hình cây
@@ -147,7 +144,7 @@ async function generateTreeRandom(count) {
   const roadWidth = 10; // Chiều rộng đường (tính từ tâm)
   const roadLength = 1000; // Chiều dài đường
   const roadCenterZ = 0; // Tâm đường theo trục Z
-  
+
   for (let i = 0; i < count; i++) {
     let positionX, positionZ;
 
@@ -168,7 +165,7 @@ async function generateTreeRandom(count) {
       await new Promise((resolve) => setTimeout(resolve, 100)); // Chờ 100ms trước khi kiểm tra lại
     }
     object = TreeCache[Math.floor(Math.random() * TreeCache.length)].clone();
-    
+
     if (!object) {
       console.error(`Không thể load mô hình ${type}`);
       continue;
@@ -196,16 +193,21 @@ const GLB_links = [
 const ObsCache = [];
 const CoinCache = [];
 
+let anim = [];
 function loadObsCoModel(GLB_links, type) {
   return new Promise((resolve, reject) => {
     if (type === "obstacle") {
-      const modelPath = GLB_links.filter((link) => link.includes("car"));
+      // const modelPath = GLB_links.filter((link) => link.includes("car"));
+      // console.log(`link ${modelPath}`);
+      const modelPath = [
+        "/GLB_Models/firefly_minecraft.glb",
+        "/GLB_Models/firefly_minecraft.glb",
+      ];
       if (modelPath.length === 0) {
         console.log(`Không tìm thấy mô hình cho ${type}.`);
         resolve(); // Trả về rỗng nếu không có mô hình nào
         return;
       }
-
       const promises = modelPath.map(
         (ObsLink) =>
           new Promise((resolve) => {
@@ -217,14 +219,16 @@ function loadObsCoModel(GLB_links, type) {
                 obs.scale.set(0.7, 0.7, 0.7);
                 ObsCache.push(obs);
                 // Tạo AnimationMixer nếu mô hình có animation
-                let mixer = new THREE.AnimationMixer(gltf.scene);
-                mixers.push(mixer);
+                // let mixer = new THREE.AnimationMixer(obs);
 
-                // Duyệt qua các hoạt hình trong mô hình (nếu có) và thêm chúng vào mixer
-                gltf.animations.forEach((clip) => {
-                  mixer.clipAction(clip).play();
-                });
-                
+                // mixers[ObsLink.includes("car_1") ? "car1" : "car2"] = mixer;
+                // // Duyệt qua các hoạt hình trong mô hình (nếu có) và thêm chúng vào mixer
+                // gltf.animations.forEach((clip) => {
+                //   mixer.clipAction(clip).play();
+                // });
+
+                anim.push(gltf.animations);
+
                 resolve(); // Hoàn thành việc tải
               },
               undefined,
@@ -234,13 +238,15 @@ function loadObsCoModel(GLB_links, type) {
               }
             );
           })
-        );
+      );
 
       Promise.all(promises).then(() => {
         console.log("Tất cả mô hình obstacle đã được tải:");
-        ObsCache.forEach((object, index) => {
-          console.log(`Phần tử obs ${index}: ${object.position.x}`, object);
+        ObsCache.forEach((obj, index) => {
+          console.log(`Phần tử obs ${index}: ${obj.position.x}`, obj);
         });
+
+        console.log(`mixer obs: ${mixers}`);
         resolve();
       });
     } else if (type === "coin") {
@@ -264,13 +270,13 @@ function loadObsCoModel(GLB_links, type) {
                 CoinCache.push(co);
 
                 // Tạo AnimationMixer nếu mô hình có animation
-                let mixer = new THREE.AnimationMixer(gltf.scene);
-                mixers.push(mixer);
+                // let mixer = new THREE.AnimationMixer(gltf.scene);
+                // mixers.push(mixer);
 
                 // Duyệt qua các hoạt hình trong mô hình (nếu có) và thêm chúng vào mixer
-                gltf.animations.forEach((clip) => {
-                  mixer.clipAction(clip).play();
-                });
+                // gltf.animations.forEach((clip) => {
+                //   mixer.clipAction(clip).play();
+                // });
 
                 resolve(); // Hoàn thành việc tải
               },
@@ -313,7 +319,10 @@ async function generateObjects(objectArray, count, minZ, maxZ, type) {
         console.log("Đang chờ mô hình obstacle...");
         await new Promise((resolve) => setTimeout(resolve, 100)); // Chờ 100ms trước khi kiểm tra lại
       }
-      object = ObsCache[Math.floor(Math.random() * ObsCache.length)].clone(); // Clone đối tượng chướng ngại vật
+      let obsRand = Math.floor(Math.random() * ObsCache.length);
+      object = ObsCache[obsRand].clone(); // Clone đối tượng chướng ngại vật
+      console.log(`anim: ${anim.length}`);
+      object.animations = anim[obsRand]; //gltf.animations
     } else if (type === "coin") {
       while (CoinCache.length === 0) {
         console.log("Đang chờ mô hình coin...");
@@ -336,7 +345,7 @@ async function generateObjects(objectArray, count, minZ, maxZ, type) {
 
     // Đảm bảo chướng ngại vật không chồng lên tiền vàng
     if (type === "obstacle") {
-      const padding = 1.5; 
+      const padding = 1.5;
       const isColliding = coins.some((coin) => {
         const coinBox = new THREE.Box3().setFromObject(coin);
         const obstacleBox = new THREE.Box3().setFromCenterAndSize(
@@ -466,8 +475,17 @@ function animate() {
 
     // Cập nhật mixer, tạo animation cho model
     mixers.forEach((mixer) => {
-        mixer.update(clock.getDelta()); // Cập nhật tất cả mixers
+      mixer.update(clock.getDelta()); // Cập nhật tất cả mixers
+      console.log(`mixer: ${mixer}`);
     });
+
+    // for (let key in mixers) {
+    //   if (mixers.hasOwnProperty(key)) {
+    //     // Kiểm tra xem key có phải là key trực tiếp của đối tượng
+    //     console.log(key, mixers[key]); // key là "player", value là "abc"
+    //     mixers[key].update(clock.getDelta());
+    //   }
+    // }
 
     // Nhân vật chạy về phía trước
     if (model.position.z > -495) {
